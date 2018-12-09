16c20a17f9291fc1f7f5925115027aa19ff7b33b
luyunfei.louis <luyunfei.louis@bytedance.com>
2018-12-09 19:51:33 +0800

    filter data

--- activeDynamics.py
+++ activeDynamics.py
@@ -3,48 +3,340 @@ import numpy as np
 import matplotlib.pyplot as plt
 import datetime
 
-userdic = {}
-prefix = '../../../Bytedance/Data/'
-suffix = '.text'
-filename = 'aweme_fans_rate_day_6'
-fr = open(prefix+filename+suffix, 'r')
+window = 30
+
+edgedic_fol = {}
+postdic_fol = {}
+actdic_fol = {}
+
+edgedic_fan = {}
+postdic_fan = {}
+actdic_fan = {}
+
+userdic_fol = {}
+userdic_fan = {}
+
+startdic_fol = {}
+startdic_fan = {}
+
+prefix = '../../../Bytedance/Data/aweme_'
+suffix1 = '_follow_day_sample.text'
+suffix2 = '_fans_day_sample.text'
+
+fr = open(prefix+'active'+suffix1, 'r')
 data = fr.readlines()
 data.sort()
+fr.close()
+start = 0
 n = len(data)
-last_total = 0
-last_social = 0
 for i in range(n):
 	temp = data[i].split('\t')
-	if not userdic.has_key(temp[0]):
-		last_total = int(temp[2])
-		last_social = int(temp[3])
-		userdic[temp[0]] = {}
-		userdic[temp[0]][temp[1]] = last_social * 1.0 / last_total
+	name = temp[0] + ':' + temp[1]
+	if temp[2] == 'null\n':
+		continue
+	if not actdic_fol.has_key(name):
+		start = datetime.datetime.strptime(str(int(temp[2])), '%Y%m%d')
+		startdic_fol[name] = start
+		actdic_fol[name] = {}
+		for j in range(window):
+			actdic_fol[name][j] = 1
 	else:
-		last_total += int(temp[2])
-		last_social += int(temp[3])
-		userdic[temp[0]][temp[1]] = last_social * 1.0 / last_total
-cnt = 0
-for k in userdic:
-	if cnt > 1000:
-		break
+		current = datetime.datetime.strptime(str(int(temp[2])), '%Y%m%d')	
+		delta = (current - start).days
+		for j in range(delta, delta+window):
+			if actdic_fol[name].has_key(j):
+				actdic_fol[name][j] += 1
+			else:
+				actdic_fol[name][j] = 1
+
+fr = open(prefix+'active'+suffix2, 'r')
+data = fr.readlines()
+data.sort()
+fr.close()
+start = 0
+n = len(data)
+for i in range(n):
+	temp = data[i].split('\t')
+	name = temp[0] + ':' + temp[1]
+	if temp[2] == 'null\n':
+		continue	
+	if not actdic_fan.has_key(name):
+		start = datetime.datetime.strptime(str(int(temp[2])), '%Y%m%d')
+		startdic_fan[name] = start
+		actdic_fan[name] = {}
+		for j in range(window):
+			actdic_fan[name][j] = 1
+	else:
+		current = datetime.datetime.strptime(str(int(temp[2])), '%Y%m%d')	
+		delta = (current - start).days
+		for j in range(delta, delta+window):
+			if actdic_fan[name].has_key(j):
+				actdic_fan[name][j] += 1
+			else:
+				actdic_fan[name][j] = 1
+
+fr = open(prefix+'edge'+suffix1, 'r')
+data = fr.readlines()
+data.sort()
+fr.close()
+n = len(data)
+social = 0
+content = 0
+start = 0
+for i in range(n):
+	temp = data[i].split('\t')
+	name = temp[0] + ':' + temp[1]
+	if temp[2] == 'null':
+		continue
+	if not edgedic_fol.has_key(name):
+		edgedic_fol[name] = {}
+		social = int(temp[3]) - int(temp[4])
+		content = int(temp[5]) - int(temp[6])
+		start = startdic_fol[name]
+		current = datetime.datetime.strptime(temp[2], '%Y%m%d')
+		if not userdic_fol.has_key(name):
+			userdic_fol[name] = temp[2]
+		tl = list()
+		tl.append(social)
+		tl.append(content)
+		delta = (current - start).days		
+		edgedic_fol[name][delta] = tl 
+	else:
+		social += int(temp[3]) - int(temp[4])
+		content += int(temp[5]) - int(temp[6])
+		tl = list()
+		tl.append(social)
+		tl.append(content)
+		current = datetime.datetime.strptime(temp[2], '%Y%m%d')		
+		delta = (current - start).days		
+		edgedic_fol[name][delta] = tl 
+
+fr = open(prefix+'edge'+suffix2, 'r')
+data = fr.readlines()
+data.sort()
+fr.close()
+n = len(data)
+social = 0
+content = 0
+start = 0
+for i in range(n):
+	temp = data[i].split('\t')
+	name = temp[0] + ':' + temp[1]
+	if temp[2] == 'null':
+		continue
+	if not edgedic_fan.has_key(name):
+		edgedic_fan[name] = {}
+		social = int(temp[3]) - int(temp[4])
+		content = int(temp[5]) - int(temp[6])
+		start = startdic_fan[name]
+		current = datetime.datetime.strptime(temp[2], '%Y%m%d')
+		if not userdic_fan.has_key(name):
+			userdic_fan[name] = temp[2]		
+		tl = list()
+		tl.append(social)
+		tl.append(content)
+		delta = (current - start).days		
+		edgedic_fan[name][delta] = tl 
+	else:
+		social += int(temp[3]) - int(temp[4])
+		content += int(temp[5]) - int(temp[6])
+		tl = list()
+		tl.append(social)
+		tl.append(content)
+		current = datetime.datetime.strptime(temp[2], '%Y%m%d')		
+		delta = (current - start).days					
+		edgedic_fan[name][delta] = tl 
+
+fr = open(prefix+'post'+suffix1, 'r')
+data = fr.readlines()
+data.sort()
+fr.close()
+start = 0
+n = len(data)
+for i in range(n):
+	temp = data[i].split('\t')
+	name = temp[0] + ':' + temp[1]
+	if not postdic_fol.has_key(name):
+		start = startdic_fol[name]
+		current = datetime.datetime.strptime(temp[2], '%Y%m%d')
+		postdic_fol[name] = {}
+		delta = (current - start).days
+		postdic_fol[name][delta] = int(temp[3])
+	else:
+		current = datetime.datetime.strptime(temp[2], '%Y%m%d')		
+		delta = (current - start).days		
+		postdic_fol[name][delta] = int(temp[3])
+
+fr = open(prefix+'post'+suffix2, 'r')
+data = fr.readlines()
+data.sort()
+fr.close()
+start = 0
+n = len(data)
+for i in range(n):
+	temp = data[i].split('\t')
+	name = temp[0] + ':' + temp[1]
+	if not postdic_fan.has_key(name):
+		start = startdic_fan[name]
+		current = datetime.datetime.strptime(temp[2], '%Y%m%d')
+		postdic_fan[name] = {}
+		delta = (current - start).days		
+		postdic_fan[name][delta] = int(temp[3])
+	else:
+		current = datetime.datetime.strptime(temp[2], '%Y%m%d')		
+		delta = (current - start).days		
+		postdic_fan[name][delta] = int(temp[3])				
+
+print 'Begin to draw followers.'
+for k in edgedic_fol:
+	#Social:y1 Content:y2
 	x = list()
-	y = list()
-	keylist = sorted(userdic[k].keys())
-	start = datetime.datetime.strptime(keylist[0], '%Y%m%d')
-	for key in keylist:
-		current = datetime.datetime.strptime(key, '%Y%m%d')
-		x.append((current-start).days)
-		y.append(userdic[k][key])
+	y1 = list()
+	y2 = list()
+	#Post
+	z1 = list()
+	w1 = list()
+	#Active
+	z2 = list()
+	w2 = list()
+
+	keylist = sorted(edgedic_fol[k].keys())
+	for d in keylist:
+		x.append(d)
+		y1.append(edgedic_fol[k][d][0])
+		y2.append(edgedic_fol[k][d][1])
+
 	x = np.array(x)
-	y = np.array(y)
-	#print x
-	#print y
-	plt.xlabel('Days')
-	plt.ylabel('Social Ratio')
-	plt.title(keylist[0])
+	y1 = np.array(y1)
+	y2 = np.array(y2)	
+
+	if not actdic_fol.has_key(k):
+		continue
+
+	keylist = sorted(actdic_fol[k].keys())
+	for d in keylist:
+		z2.append(d)
+		w2.append(actdic_fol[k][d])
+
+	z2 = np.array(z2)	
+	w2 = np.array(w2)
+
+	fig, ax1 = plt.subplots()
+	ax2 = ax1.twinx()
 	plt.grid()
-	plt.plot(x, y, marker='.')
-	plt.savefig('../../../Bytedance/Figs/daily_dynamics/fans_6/'+k+'.png')
+	plt.title(userdic_fol[k])
+	ax1.plot(x, y1, marker='.', c='b', label='Social')
+	ax1.plot(x, y2, marker='.', c='g', label='Content')
+	ax1.set_xlabel('Days')
+	ax1.set_ylabel('Pop')
+	plt.legend(loc=1)
+	ax2.plot(z2, w2, marker='.', c='r', label='Active')
+	ax2.set_ylabel('Activeness')
+	plt.legend(loc=2)
+	plt.savefig('../../../Bytedance/Figs/active_dynamics/fol_act/'+k+'.png')
 	plt.cla()
-	cnt += 1
+	plt.close(fig)
+
+	if not postdic_fol.has_key(k):
+		continue
+
+	keylist = sorted(postdic_fol[k].keys())
+	for d in keylist:
+		z1.append(d)
+		w1.append(postdic_fol[k][d])
+
+	z1 = np.array(z1)
+	w1 = np.array(w1)
+
+	fig, ax1 = plt.subplots()
+	ax2 = ax1.twinx()
+	plt.grid()
+	plt.title(userdic_fol[k])
+	ax1.plot(x, y1, marker='.', c='b', label='Social')
+	ax1.plot(x, y2, marker='.', c='g', label='Content')
+	ax1.set_xlabel('Days')
+	ax1.set_ylabel('Pop')
+	plt.legend(loc=1)
+	ax2.plot(z1, w1, marker='.', c='r', label='Post')
+	ax2.set_ylabel('Posts')
+	plt.legend(loc=2)
+	plt.savefig('../../../Bytedance/Figs/active_dynamics/fol_post/'+k+'.png')
+	plt.cla()
+	plt.close(fig)
+
+print 'Begin to draw fans.'
+for k in edgedic_fan:
+	#Social:y1 Content:y2
+	x = list()
+	y1 = list()
+	y2 = list()
+	#Post
+	z1 = list()
+	w1 = list()
+	#Active
+	z2 = list()
+	w2 = list()
+
+	keylist = sorted(edgedic_fan[k].keys())
+	for d in keylist:
+		x.append(d)
+		y1.append(edgedic_fan[k][d][0])
+		y2.append(edgedic_fan[k][d][1])
+
+	x = np.array(x)
+	y1 = np.array(y1)
+	y2 = np.array(y2)	
+
+	if not actdic_fan.has_key(k):
+		continue
+
+	keylist = sorted(actdic_fan[k].keys())
+	for d in keylist:
+		z2.append(d)
+		w2.append(actdic_fan[k][d])
+
+	z2 = np.array(z2)	
+	w2 = np.array(w2)
+
+	fig, ax1 = plt.subplots()
+	ax2 = ax1.twinx()
+	plt.grid()
+	plt.title(userdic_fan[k])
+	ax1.plot(x, y1, marker='.', c='b', label='Social')
+	ax1.plot(x, y2, marker='.', c='g', label='Content')
+	ax1.set_xlabel('Days')
+	ax1.set_ylabel('Pop')
+	plt.legend(loc=1)
+	ax2.plot(z2, w2, marker='.', c='r', label='Active')
+	ax2.set_ylabel('Activeness')
+	plt.legend(loc=2)
+	plt.savefig('../../../Bytedance/Figs/active_dynamics/fans_act/'+k+'.png')
+	plt.cla()
+	plt.close(fig)
+
+	if not postdic_fan.has_key(k):
+		continue
+
+	keylist = sorted(postdic_fan[k].keys())
+	for d in keylist:
+		z1.append(d)
+		w1.append(postdic_fan[k][d])
+
+	z1 = np.array(z1)
+	w1 = np.array(w1)
+
+	fig, ax1 = plt.subplots()
+	ax2 = ax1.twinx()
+	plt.grid()
+	plt.title(userdic_fan[k])
+	ax1.plot(x, y1, marker='.', c='b', label='Social')
+	ax1.plot(x, y2, marker='.', c='g', label='Content')
+	ax1.set_xlabel('Days')
+	ax1.set_ylabel('Pop')
+	plt.legend(loc=1)
+	ax2.plot(z1, w1, marker='.', c='r', label='Post')
+	ax2.set_ylabel('Posts')
+	plt.legend(loc=2)
+	plt.savefig('../../../Bytedance/Figs/active_dynamics/fans_post/'+k+'.png')
+	plt.cla()
+	plt.close(fig)
---
+++ filterData.py
@@ -0,0 +1,73 @@
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime
+
+def returnType(a, b):
+	a = int(a)
+	b = int(b)
+	# Many fans and little followers
+	if (a == 1 or a == 4 or a == 7) and (b == 3 or b == 6 or b == 9):
+		return 0
+	# Many followers and little fans
+	if (b == 1 or b == 4 or b == 7) and (a == 3 or a == 6 or a == 9):
+		return 1
+	# Others
+	return -1
+
+fr = open('../aweme_edge_common_day_sample.text', 'r')
+data = fr.readlines()
+data.sort()
+fr.close()
+result = list()
+n = len(data)
+for i in range(n):
+	temp = data[i].split('\t')
+	cluster = returnType(temp[-2], temp[-1])
+	if cluster < 0:
+		continue
+	line = temp[0] + '\t' + str(cluster) + '\t' + temp[1] + '\t' + temp[2] + '\t' + temp[3] + '\t' + temp[4] + '\t' + temp[5] + '\n'
+	result.append(line)
+
+fw = open('../aweme_edge_common_day_sample_pol.text', 'w')
+for line in result:
+	fw.write(line)
+fw.close()
+
+fr = open('../aweme_active_common_day_sample.text', 'r')
+data = fr.readlines()
+data.sort()
+fr.close()
+result = list()
+n = len(data)
+for i in range(n):
+	temp = data[i].split('\t')
+	cluster = returnType(temp[-2], temp[-1])
+	if cluster < 0:
+		continue
+	line = temp[0] + '\t' + str(cluster) + '\t' + temp[1] + '\n'
+	result.append(line)
+
+fw = open('../aweme_active_common_day_sample_pol.text', 'w')
+for line in result:
+	fw.write(line)
+fw.close()
+
+fr = open('../aweme_post_common_day_sample.text', 'r')
+data = fr.readlines()
+data.sort()
+fr.close()
+result = list()
+n = len(data)
+for i in range(n):
+	temp = data[i].split('\t')
+	cluster = returnType(temp[-2], temp[-1])
+	if cluster < 0:
+		continue
+	line = temp[0] + '\t' + str(cluster) + '\t' + temp[1] + '\t' + temp[2] + '\n'
+	result.append(line)
+
+fw = open('../aweme_post_common_day_sample_pol.text', 'w')
+for line in result:
+	fw.write(line)
+fw.close()
