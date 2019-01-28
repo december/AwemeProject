6c6764efad47f1f0ebd1da5e4ba1b1754fbc22e3
luyunfei.louis <luyunfei.louis@bytedance.com>
2019-01-28 15:03:41 +0800

    change learning rate

--- scModel.py
+++ scModel.py
@@ -6,7 +6,7 @@ import scipy as sp
 import numpy as np
 import tensorflow as tf
 
-alpha = [1e-10, 1e-10, 1e-8, 1e-10, 1e-10, 1e-10, 1e-10]
+alpha = [1e-9, 1e-9, 1e-8, 1e-8, 1e-8, 1e-8, 1e-8]
 total = 10000
 threshold = 0.0001
 
@@ -165,7 +165,7 @@ for i in range(n):
 			ncdic[info][int(temp[5])] = int(temp[6])		
 
 cnt = 0
-p = [0.05, 0.05, 0.15, -0.01, -0.01, -0.01, -0.01] #a, b, theta, k1, k2, k3, k4
+p = [0.05, 0.05, 0.15, 0, 0, 0, 0] #a, b, theta, k1, k2, k3, k4
 lastObj = LnObj(cdic, ncdic, p)
 while cnt < total:
 	p = GradDes(cdic, ncdic, p, alpha)
