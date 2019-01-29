9b73fcc771b64951bb7b05397ce7a0a234ebfe32
luyunfei.louis <luyunfei.louis@bytedance.com>
2019-01-29 20:25:24 +0800

    change initial

--- scModel.py
+++ scModel.py
@@ -6,7 +6,7 @@ import scipy as sp
 import numpy as np
 import tensorflow as tf
 
-alpha = [1e-10, 1e-11, 1e-7, 1e-7, 1e-7, 1e-7, 1e-7]
+alpha = [1e-11, 1e-11, 1e-7, 1e-7, 1e-7, 1e-7, 1e-7]
 total = 10000
 threshold = 0.0001
 
